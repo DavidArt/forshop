f34c2e8b0255ca990f2f260fad6ab65f8934b501
Rus David <davidrus.art@gmail.com>
2017-08-08 13:12:49 +0300

    initial commit. new spring-boot project

---
+++ src/main/java/com/davidrus/forshop/ForshopApplication.java
@@ -0,0 +1,12 @@
+package com.davidrus.forshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForshopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/davidrus/forshop/ForshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davidrus.forshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
